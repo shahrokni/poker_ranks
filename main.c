3ef526d32f96eefe93b7d9cf2bfbf98b99ca4745
Seyed Mahmoud Shahrokni <mahmoud.shahrokni@celus.io>
2024-09-08 17:34:06 +0200

    feat: rest of ranking functions were added

--- main.c
+++ main.c
@@ -4,6 +4,17 @@
 
 #define TRUE 1
 #define FALSE 0
+#define NVP "NOT A VALID PATTERN"
+#define RF "ROYAL FLUSH"
+#define SF "STRAIGHT FLUSH"
+#define FOK "FOUR OF A KIND"
+#define FH "FULL HOUSE"
+#define F "FLUSH"
+#define S "STRAIGHT"
+#define TOK "THREE OF A KIND"
+#define TP "TWO PARIS"
+#define P "PAIR"
+#define HG "HIGH CARD"
 
 struct Card
 {
@@ -93,8 +104,8 @@ short is_royal_flush(const struct Card *cards)
         we can assert royal flush exists
      */
 
-    /* 0 > S 1 > H 2 > C 3 > D */
-    short scores[4] = {0, 0, 0, 0};
+    /* 0=>S 1=>H 2=>C 3=>D */
+    short scores[4] = {0};
 
     for (short j = 0; j < 7; j += 1)
     {
@@ -218,14 +229,14 @@ short is_full_house(const struct Card *cards)
         We use a bucket to count the cards per rank
         The rule is that four of a kind can't be split into smaller groups.
         Thus, if you have four cards of the same rank,
-        you can't consider part of it as a three of a kind and another part as a pair.      
+        you can't consider part of it as a three of a kind and another part as a pair.
      */
-    short bucket[13] = {0};
+    short ranks[13] = {0};
 
     for (short i = 0; i < 7; i += 1)
     {
-        short bucket_index = cards[i].rank - 2;
-        bucket[bucket_index] += 1;
+        short rank_index = cards[i].rank - 2;
+        ranks[rank_index] += 1;
     }
 
     short has_3_kind = FALSE;
@@ -233,90 +244,160 @@ short is_full_house(const struct Card *cards)
 
     for (short i = 0; i < 13; i += 1)
     {
-        if(!has_3_kind)
-            has_3_kind = bucket[i] == 3;
-        if(!has_pair)
-            has_pair = bucket[i] == 2;
+        if (!has_3_kind)
+            has_3_kind = ranks[i] == 3;
+        if (!has_pair)
+            has_pair = ranks[i] == 2;
 
         if (has_3_kind && has_pair)
             return TRUE;
-    }  
+    }
 
     return FALSE;
 }
 
-void calculate_poker_rank(char *id, char *combination)
+short is_flush(const struct Card *cards)
+{
+    /* 0=>S 1=>H 2=>C 3=>D */
+    short suits_count[4] = {0};
+
+    for (short i = 0; i < 7; i += 1)
+    {
+        char suit = cards[i].suit;
+
+        if (suit == 'S')
+            suits_count[0] += 1;
+        else if (suit == 'H')
+            suits_count[1] += 1;
+        else if (suit == 'C')
+            suits_count[2] += 1;
+        else
+            suits_count[3] += 1;
+
+        if (suits_count[0] == 5 || suits_count[1] == 5 || suits_count[2] == 5 || suits_count[3] == 5)
+            return TRUE;
+    }
+
+    return FALSE;
+}
+
+short is_straight(const struct Card *cards)
+{
+    short ranks[13] = {0};
+    short count_consecutive_ranks = 0;
+
+    for (short i = 0; i < 7; i += 1)
+    {
+        ranks[cards[i].rank - 2] = 1;
+    }
+
+    for (short i = 0; i < 13; i += 1)
+    {
+        if (ranks[i])
+            count_consecutive_ranks += 1;
+        else
+            count_consecutive_ranks = 0;
+
+        if (count_consecutive_ranks == 5)
+            return TRUE;
+    }
+
+    return FALSE;
+}
+
+short is_three_kind(const struct Card *cards)
+{
+    short ranks[13] = {0};
+
+    for (short i = 0; i < 7; i += 1)
+    {
+        ranks[cards[i].rank - 2] += 1;
+    }
+
+    for (short i = 0; i < 13; i += 1)
+    {
+        if (ranks[i] == 3)
+            return TRUE;
+    }
+
+    return FALSE;
+}
+
+short is_x_pairs(const struct Card *cards, const short expected_pair)
+{
+    short ranks[13] = {0};
+
+    for (short i = 0; i < 7; i += 1)
+    {
+        ranks[cards[i].rank - 2] += 1;
+    }
+
+    short count_pair = 0;
+
+    for (short i = 0; i < 13; i += 1)
+    {
+        if (ranks[i] == 2)
+            count_pair += 1;
+
+        if (count_pair == expected_pair)
+            return TRUE;
+    }
+
+    return FALSE;
+}
+
+char *calculate_poker_rank(char *id, char *combination)
 {
     if (!is_pattern_valid(combination))
     {
         printf("The pattern %s is not valid! \n", combination);
-        return;
+        return NVP;
     }
 
     struct Card *cards = convert_combination_to_cards(combination);
 
-    drw_line();
-    printf("%s is being checked ... \n", combination);
-
     /* ROYAL FLUSH CHECK */
     if (is_royal_flush(cards))
     {
-        printf("%s > ROYAL FLUSH WAS FOUND: %s \n", id, combination);
-        drw_line();
-
-        return;
+        return RF;
     }
     else if (is_straight_flush(cards))
     {
-        printf("%s > STRAIGHT FLUSH WAS FOUND: %s \n", id, combination);
-        drw_line();
-
-        return;
+        return SF;
     }
     else if (is_four_kind(cards))
     {
-        printf("%s > 4 OF A KIND WAS FOUND: %s \n", id, combination);
-        drw_line();
-
-        return;
+        return FOK;
     }
     else if (is_full_house(cards))
     {
-        printf("%s > FULL HOUSE WAS FOUND: %s \n", id, combination);
-        drw_line();
-
-        return;
+        return FH;
+    }
+    else if (is_flush(cards))
+    {
+        return F;
+    }
+    else if (is_straight(cards))
+    {
+        return S;
+    }
+    else if (is_three_kind(cards))
+    {
+        return TOK;
+    }
+    else if (is_x_pairs(cards, 2))
+    {
+        return TP;
+    }
+    else if (is_x_pairs(cards, 1))
+    {
+        return P;
     }
 
-    printf("%s > No pattern was found \n", id);
-    drw_line();
+    return HG;
 }
 
 int main()
 {
-    printf("POKER RANKING! \n");
-    printf("-------------- \n");
-    /* TEST ROYAL FLUSH */
-    calculate_poker_rank("1", "14H13H12H11H10H03D02S");
-    calculate_poker_rank("2", "14H12H13H10H11H03D02S");
-    calculate_poker_rank("3", "03D14H12H13H10H02S11H");
-
-    /* TEST STRAIGHT FLUSH */
-    calculate_poker_rank("4", "08H07H06H05H04H03D02S");
-    calculate_poker_rank("5", "03D12S04D05D06D11C07D");
-    calculate_poker_rank("6", "04S02D02S03S14D06S05S");
-    calculate_poker_rank("7", "09S10S11S12S13S04D05D");
-    calculate_poker_rank("8", "08H07H06H05H04D03D02S");
-
-    /* TEST 4 OF A KIND*/
-    calculate_poker_rank("9", "05S05D05C05H14D12S12D");
-    calculate_poker_rank("10", "12S05D12H03D12C02D12D");
-    calculate_poker_rank("11", "02H03H04H13S13H13C13D");
-    calculate_poker_rank("12", "02S02H10D11D12D02C02D");
-
-    /* TEST FULL HOUSE */
-    calculate_poker_rank("13", "13H13D13S05H05C02D02C");
-    calculate_poker_rank("13", "02S02D02H03S03D03H12D");
-
     return 0;
 }
